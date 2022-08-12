e4f9e6d469fc78554efe11488c1150929cc7948d
Edmh1 <103791960+Edmh1@users.noreply.github.com>
2022-08-11 19:05:21 -0500

    cambio 1

--- index.py
+++ index.py
@@ -1,1 +1,22 @@
-print("hello world")
+
+def restablecer_contraseña():
+    clave1 = str(input("Nueva contraseña:  "))
+    contador = 0
+    contador2 = 2
+    while contador < 3:
+        clave2 = str(input("confirmar contraseña:  "))
+        if clave1 == clave2:
+            print("La clave ha sido cambiada con exito")
+            exit(0)
+        else:
+            if contador2 == 0:
+                print(f"Las contraseñas no coinciden, no tienes intentos restantes")
+                break
+            else:
+                print(f"Las contraseñas no coinciden, quedan {contador2} intentos ")
+        contador+= 1
+        contador2 -= 1
+    print("Han acabado tus opciones")
+
+restablecer_contraseña()
+
